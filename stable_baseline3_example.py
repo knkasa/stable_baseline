cf2b6be5097076eaf7a209288f906516def52e31
Ken Nakatsukasa <knkasa@gmail.com>
2025-02-10 13:11:46 +0900

    Update stable_baseline3_example.py

--- stable_baseline3_example.py
+++ stable_baseline3_example.py
@@ -1,3 +1,6 @@
+# Example of diffusion movie from scratch.
+# https://levelup.gitconnected.com/building-a-tiny-text-to-video-model-from-scratch-using-python-f31bdab12fbb
+
 import gym
 from stable_baselines3 import PPO
 
